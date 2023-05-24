e7b3a7bb698e5eafb09f67c4244b44d17af13a0d
afpre <afpre@DESKTOP-8NN0FJV>
2023-05-24 22:30:15 +0200

    Zadanie domowe dzien 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Ewa";
+bool famale = true;
+int age = 29;
+
+
+if (famale == true)
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta ponizej 33 lat");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
