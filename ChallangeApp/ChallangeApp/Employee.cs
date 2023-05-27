06efebd77a97f9aab87b2521aa452c5b7ac3e511
afpre <afpre@DESKTOP-8NN0FJV>
2023-05-28 00:10:41 +0200

    Zadanie domowe dzien 6

---
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace ChallangeApp
+
+{
+
+    public class Employee
+    {
+        private List<int> Grands = new List<int>();
+
+
+        public Employee(string FirstName, string LastName, int Age)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+
+
+        public int Result
+        {
+            get
+            {
+                return this.Grands.Sum();
+            }
+        }
+
+        public void AddGrands(int number)
+        {
+            this.Grands.Add(number);
+        }
+    }
+}
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,20 +1,61 @@
-﻿int number = 5888452;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+﻿using ChallangeApp;
 
-Console.WriteLine("Wyniki dla liczby: 5888452");
+Employee Employee1 = new Employee("Zenek", "Len", 32);
+Employee Employee2 = new Employee("Bogdan", "Pro", 40);
+Employee Employee3 = new Employee("Filip", "Dran", 23);
 
-int[] Liczbylicz = new int[10];
+// 11
+Employee1.AddGrands(9);
+Employee1.AddGrands(5);
+Employee1.AddGrands(3);
+Employee1.AddGrands(8);
+Employee1.AddGrands(7);
+// 12
+Employee2.AddGrands(4);
+Employee2.AddGrands(10);
+Employee2.AddGrands(7);
+Employee2.AddGrands(9);
+Employee2.AddGrands(3);
+// 26
+Employee3.AddGrands(9);
+Employee3.AddGrands(3);
+Employee3.AddGrands(3);
+Employee3.AddGrands(4);
+Employee3.AddGrands(6);
 
-foreach (var Liczby in letters)
+
+var result1 = Employee1.Result;
+var result2 = Employee2.Result;
+var result3 = Employee3.Result;
+
+Console.WriteLine($"{Employee1.FirstName} {Employee1.LastName} | Uzyskał: {result1} pkt");
+Console.WriteLine($"{Employee2.FirstName} {Employee2.LastName} | Uzyskał: {result2} pkt");
+Console.WriteLine($"{Employee3.FirstName} {Employee3.LastName} | Uzyskał: {result3} pkt");
+Console.WriteLine("");
+
+
+if (result1 > result2 && result1 > result3)
 {
-    if (Liczby >= '0' && Liczby <='9')
-    {
-        int wartość = Liczby - '0';
-        Liczbylicz[wartość]++;
-    }
+
+    Console.WriteLine($"Pracownik z najlepszą oceną:{Employee1.FirstName} {Employee1.LastName}");
+    Console.WriteLine($"Wiek: {Employee1.Age} lat");
+    Console.WriteLine($"Wynik: {result1}");
+
 }
-for (int i = 0; i< Liczbylicz.Length; i++)
+else if (result2 > result1 && result2 > result3)
 {
-    Console.WriteLine("{0} => {1}", i, Liczbylicz[i]);
+    Console.WriteLine($"Pracownik z najlepszą oceną:{Employee2.FirstName} {Employee2.LastName}");
+    Console.WriteLine($"Wiek: {Employee2.Age} lat");
+    Console.WriteLine($"Wynik: {result2}");
+
 }
+else
+{
+    Console.WriteLine($"Pracownik z najlepszą oceną:{Employee3.FirstName} {Employee3.LastName}");
+    Console.WriteLine($"Wiek: {Employee3.Age} lat");
+    Console.WriteLine($"Wynik: {result3}");
+
+}
+
+
+
